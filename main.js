b18132f53a5801e750af445367cbfa4f66b26084
Vincent L <vincentliang0208@gmail.com>
2024-06-27 13:38:53 -0600

    Benefits page visual update

--- main.js
+++ main.js
@@ -69,21 +69,3 @@ function myFunction() {
     }
   }
 
-// animation 
-
-
-/*const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const square = entry.target.querySelector('.square');
-  
-      if (entry.isIntersecting) {
-        square.classList.add('square-animation');
-        return; // if we added the class, exit the function
-      }
-  
-      // We're not intersecting, so remove the class!
-      square.classList.remove('square-animation');
-    });
-  });
-  
-  observer.observe(document.querySelector('.company-overview-1 img')); */
