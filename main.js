e3c2e1acf4dc6d6300fffd451b74acac71814bd7
AllenLiang31 <89212124+AllenLiang31@users.noreply.github.com>
2024-06-27 23:56:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -69,4 +69,118 @@ function myFunction() {
     }
   }
 
+  //job opening, creating objects for each job
+  const jobs =[
+    {
+        title:"Stir Fry Chef ($17/hr)",
+        image:"pictures/Image for stir fry chef.png",
+        details:"#",
+        openPosition:"#"
+        
+      },
+    
+      {
+        title:"Fryer Chef ($17/hr)",
+        image:"pictures/Image for fryer chef.png",
+        details:"#",
+        openPosition:"#"
+    
+      },
+      {
+        title:"Preparation Chef ($17/hr)",
+        image:"pictures/Image for prep chef.png",
+        details:"#",
+        openPosition:"#"
+    
+      },
+      {
+        title:"Customer service specialist ($16/hr)",
+        image:"pictures/Image for customer service spec.png",
+        details:"#",
+        openPosition:"#"
+    
+      },
+      {
+        title:"Driver ($10/hr + tips)",
+        image:"pictures/Image for driver.png",
+        details:"#",
+        openPosition:"#"
+    
+      },
+      {
+        title:"Waiter/Waitress ($16/hr)",
+        image:"pictures/Image for waiter.png",
+        details:"#",
+        openPosition:"#"
+    
+      },
+
+  ];
+  
+
+  const jobsHeading = document.querySelector(".jobs-list-container h2");
+const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+const jobSearch = document.querySelector(".jobs-list-container .job-search");
+
+let searchTerm = "";
+
+if (jobs.length == 1) {
+  jobsHeading.innerHTML = `${jobs.length} Job`;
+} else {
+  jobsHeading.innerHTML = `${jobs.length} Jobs`;
+}
+
+const createJobListingCards = () => {
+  jobsContainer.innerHTML = "";
+
+  jobs.forEach((job) => {
+    if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      let jobCard = document.createElement("div");
+      jobCard.classList.add("job");
+
+      let image = document.createElement("img");
+      image.src = job.image;
+
+      let title = document.createElement("h3");
+      title.innerHTML = job.title;
+      title.classList.add("job-title");
+
+      let details = document.createElement("div");
+      details.innerHTML = job.details;
+      details.classList.add("details");
+
+      let detailsBtn = document.createElement("a");
+      detailsBtn.href = job.link;
+      detailsBtn.innerHTML = "More Details";
+      detailsBtn.classList.add("details-btn");
+
+      let openPositions = document.createElement("span");
+      openPositions.classList.add("open-positions");
+
+      if (job.openPositions == 1) {
+        openPositions.innerHTML = `${job.openPositions} open position`;
+      } else {
+        openPositions.innerHTML = `${job.openPositions} open positions`;
+      }
+
+      jobCard.appendChild(image);
+      jobCard.appendChild(title);
+      jobCard.appendChild(details);
+      jobCard.appendChild(detailsBtn);
+      jobCard.appendChild(openPositions);
+
+      jobsContainer.appendChild(jobCard);
+    }
+  });
+};
+
+createJobListingCards();
+
+jobSearch.addEventListener("input", (e) => {
+  searchTerm = e.target.value;
+
+  createJobListingCards();
+});
+  
+
 
